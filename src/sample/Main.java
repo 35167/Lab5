bc855f85e180461bd0043c85b8cc71425f6245d3
35167 <35167@studentwsei.pl>
2021-03-19 08:49:34 +0100

    2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -11,14 +12,18 @@ import javafx.scene.paint.Color;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Canvas canvas = new Canvas(800, 600);
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+    @Override
+    public void start(Stage stage){
+        Canvas canvas = new Canvas(800, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        stage.setTitle("Kulki!");
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
+
+        gc.setFill(Color.BLACK);
+        gc.fillRect(50, 50, 700, 500);
     }
 
 
